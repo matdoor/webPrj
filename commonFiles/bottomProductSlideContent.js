c0c7ac89285e0d1f2ce0dfa97fdd758c2fc25bf3
ken <ken.ning@leandev.cn>
2016-08-01 13:44:05 +0800

    aaa

---
+++ commonFiles/bottomProductSlideContent.js
@@ -0,0 +1,8 @@
+Fai.top.Photo334 = {};
+Fai.top.Photo334.ieOpt = {"effType":6,"borderType":false,"borderColor":"#000","borderWidth":1,"borderStyle":1,"style":16,"fullMaskCusBg":false,"fullMaskCusName":false,"fullMaskCusDisc":false,"fullMaskOpenDisc":false};
+Fai.top.Photo334.tgOpt = {"nameHidden":false,"nameWordWrap":false,"targetParent":"photoMarqueeForm","target":"imgDiv"};
+Fai.top.Photo334.callbackArgs = [];
+Site.loadPhotoMarquee(334, true, false, 0);
+Fai.top.photoSlideTmp334 = {};
+Fai.top.photoSlideTmp334 = {"photoSlideId":"photoSlide_334","photoSlideType":0,"photoDataList":[{"id":1,"name":"P1","basic":"","pic":"P1","picPath":"./commonFiles/bottomScrollProductList/P1.jpg","picWidth":480,"picHeight":480},{"id":2,"name":"P2","basic":"","pic":"P2","picPath":"./commonFiles/bottomScrollProductList/P2.jpg","picWidth":480,"picHeight":480}],"pluginsOptions":{"closeBtn":{"on":true,"arg":{}},"prevBtn":{"on":true,"arg":{}},"nextBtn":{"on":true,"arg":{}},"descPanel":{"on":true,"arg":{}}}};
+Fai.top.changeMarquee334 = function(){Fai.stopInterval('marquee334');Site.loadPhotoMarquee(334, true, false, 0);};
